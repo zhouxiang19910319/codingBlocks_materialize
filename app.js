149df1a6ca696b8c9cd0aef023de7855c4d153c9
zhouxiang19910319 <zhouxiang19910319@gmail.com>
2020-08-11 23:30:34 +0800

    finish Podcasts left section

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Tabs
-window.addEventListener("DOMContentLoaded", event => {
+window.addEventListener("DOMContentLoaded", (event) => {
   const options = {
     duration: 300,
     onShow: null,
     swipeable: false,
-    responsiveThreshold: Infinity
+    responsiveThreshold: Infinity,
   };
 
   const tabsContainer = document.querySelector(".nav-content .tabs");
@@ -12,53 +12,52 @@ window.addEventListener("DOMContentLoaded", event => {
 });
 
 // Side Nav
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
   const options = {
-    inDuration:250,
-    outDuration:250,
-    edge:'left'
-  }
+    inDuration: 250,
+    outDuration: 250,
+    edge: "left",
+  };
   var instances = M.Sidenav.init(elems, options);
 });
 
-
 // Carousel
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
   const options = {
     fullWidth: true,
-    indicators: true
-  }
+    indicators: true,
+  };
   var instances = M.Carousel.init(elems, options);
 });
 
 // Collapsible
-document.addEventListener('DOMContentLoaded', function () {
-  var elem = document.querySelector('.collapsible.expandable');
+document.addEventListener("DOMContentLoaded", function () {
+  var elem = document.querySelector(".collapsible.expandable");
   var instance = M.Collapsible.init(elem, {
-    accordion: false
+    accordion: false,
   });
   const options = {
     inDuration: 200,
-    outDuration: 200
-  }
+    outDuration: 200,
+  };
 });
 
 // Floating action button
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
   const options = {
-    hoverEnabled: false
-  }
+    hoverEnabled: false,
+  };
   var instances = M.FloatingActionButton.init(elems, options);
 });
 
 //ToolTips
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.tooltipped');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".tooltipped");
   const options = {
-    position : 'left'
-  }
+    position: "left",
+  };
   var instances = M.Tooltip.init(elems, options);
 });
